8ad7b1f9907a39c3802fe6fa086d6abaaa3915d9
nima248 <none>
2026-01-20 15:11:13 +1030

    Replace ogg with opus, include synth tones
    Build steps updated for reworking directory
    structure to include different audio files categories

--- build/build.py
+++ build/build.py
@@ -4,35 +4,70 @@
 Simple build script to run before deployment
 '''
 
+import tone_generator
 from pathlib import Path
 import json
 
-# Generate audio file manifest
-FILETYPES = ['mp3', 'ogg']
-audio_dir = Path(__file__).resolve().parent.parent / 'assets' / 'audio'
-save_file = audio_dir / 'audio-files.json'
+def generate_audio_file_manifest(audio_dir):
+    FILE_FORMATS = ['mp3', 'opus']
+    save_file = audio_dir / 'audio-files.json'
 
-data = {}
-for ft in FILETYPES:
-    dir = audio_dir / ft
-    files = [
-        f.name
-        for f in sorted(dir.iterdir())
-        if f.is_file() and f.suffix.lower() == f'.{ft}'
-    ]
+    data = {}
+    for format in FILE_FORMATS:
+        top_dir = audio_dir / format
 
-    this_ft = {}
-    for file in files:
-        note, _ = file.split('-') if '-' in file else file.split('.')
-        if note not in this_ft.keys():
-            this_ft[note] = []
-        this_ft[note].append(file)
-    data[ft] = this_ft
+        dirs = [
+            top_dir / d.name
+            for d in sorted(top_dir.iterdir())
+            if d.is_dir()
+        ]
+        print(f'{format} dirs:')
+        for d in dirs:
+            print(f'\t{d.name}')
 
-with save_file.open('w', encoding='utf-8') as f:
-    json.dump(data, f, indent=2)
+        this_format = {}
+        for category_dir in dirs:
+            this_category = {}
+            files = [
+                f.name
+                for f in sorted(category_dir.iterdir())
+                if f.is_file() and f.suffix.lower() == f'.{format}'
+            ]
 
-print(f'Saved {len(data)} filetypes to {save_file}:')
-for ft, notes in data.items():
-    n_files = sum(len(files) for note, files in notes.items())
-    print(f'  - {ft}: {len(notes)} notes, {n_files} files')
+            for file in files:
+                note, _ = file.split('-') if '-' in file else file.split('.')
+                if note not in this_category.keys():
+                    this_category[note] = []
+                this_category[note].append(file)
+            this_format[category_dir.name] = this_category
+        data[format] = this_format
+
+    with save_file.open('w', encoding='utf-8') as f:
+        json.dump(data, f, indent=2)
+
+    print(f'Saved {len(data)} formats to {save_file}:')
+    for format, categories in data.items():
+        n_notes = 0
+        n_files = 0
+        for _, notes in categories.items():
+            n_notes += len(notes)
+            for note, files in notes.items():
+                n_files += len(files)
+        print(f'  - {format}: {len(categories)} categories, {n_notes} notes, {n_files} files')
+
+
+def main():
+    MAKE_SYNTH_TONES = False
+
+    audio_dir = Path(__file__).resolve().parent.parent / 'assets' / 'audio'
+
+    if MAKE_SYNTH_TONES:
+        print('\n1. making synth tones')
+        tone_generator.make_synth_tones(audio_dir)
+
+    print('\n2. generating audio manifest')
+    generate_audio_file_manifest(audio_dir)
+
+
+if __name__ == '__main__':
+    main()
---
+++ build/tone_generator.py
@@ -0,0 +1,112 @@
+#!/bin/env python3
+
+import math
+import numpy as np
+import pathlib
+import soundfile as sf
+import subprocess
+import tempfile
+
+NOTES = {
+    'C': -9,
+    'Csh': -8,
+    'D': -7,
+    'Dsh': -6,
+    'E': -5,
+    'F': -4,
+    'Fsh': -3,
+    'G': -2,
+    'Gsh': -1,
+    'A': 0,
+    'Ash': 1,
+    'B': 2, 
+}
+
+SR = 44100
+DURATION = 1.0
+HARMONICS = 20  # number of partials
+FREQ_CUTOFF = 5000
+
+
+def make_tone(note, octave, decay_power, opus_dir=None, mp3_dir=None):
+    if note not in NOTES.keys():
+        raise ValueError(f'Bad note: {note}')
+    freq = 440 * 2**(((octave - 4)*12 + NOTES[note]) / 12)
+
+    if not (opus_dir or mp3_dir):
+        raise ValueError('Must give either opus_dir or mp3_dir')
+
+    # set duration to an integer number of periods, for clean looping
+    period = 1 / freq
+    duration = math.floor(DURATION / period) * period
+
+    t = np.linspace(0, duration, int(SR * duration), False)
+    signal = np.zeros_like(t)
+
+    for n in range(1, HARMONICS + 1):
+        if freq * n > FREQ_CUTOFF:
+            print(f'Frequency {freq * n} skipped')
+            break
+        amplitude = 1 / n**decay_power
+        signal += amplitude * np.sin(2 * np.pi * freq * n * t)
+
+    signal /= np.max(np.abs(signal))
+    filename = f"{note}{octave}"
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempwav = pathlib.Path(tempdir) / 'temp.wav'
+        sf.write(tempwav, signal, SR)
+        if opus_dir:
+            subprocess.run([
+                "ffmpeg", "-y",
+                "-i", tempwav,
+                "-c:a", "libopus",
+                "-b:a", "256k",
+                f"{opus_dir}/{filename}.opus"
+            ], check=True)
+        if mp3_dir:
+            subprocess.run([
+                "ffmpeg",
+                "-y",
+                "-i", tempwav,
+                "-codec:a", "libmp3lame",
+                "-b:a", "320k",
+                f"{mp3_dir}/{filename}.mp3"
+            ], check=True)
+
+
+def make_synth_tones(output_dir):
+    decay_power = 1.25
+
+    START_NOTE = 'C'
+    START_OCTAVE = 2
+
+    END_NOTE = 'A'
+    END_OCTAVE = 3
+
+    note_letters = list(NOTES)
+    note_i = note_letters.index(START_NOTE)
+    octave = START_OCTAVE
+
+    opus_dir = pathlib.Path(output_dir) / './opus' / f'{decay_power:.2f}'
+    mp3_dir = pathlib.Path(output_dir) / './mp3' / f'{decay_power:.2f}'
+
+    pathlib.Path.mkdir(opus_dir, parents=True, exist_ok=True)
+    pathlib.Path.mkdir(mp3_dir, parents=True, exist_ok=True)
+
+    print(f'Saving to {opus_dir} and {mp3_dir}')
+
+    while True:
+        note = note_letters[note_i]
+        print(f'{note}{octave}')
+        make_tone(note, octave, decay_power, opus_dir=opus_dir, mp3_dir=mp3_dir)
+        if note == END_NOTE and octave == END_OCTAVE:
+            break
+        note_i = (note_i + 1) % len(note_letters)
+        if note_i == 0:
+            octave += 1
+
+
+
+if __name__ == '__main__':
+    print('Running main')
+    make_synth_tones('./')
